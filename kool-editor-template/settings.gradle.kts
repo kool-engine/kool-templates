c7537f6c7fd9f54da2f82f04d29b0d5d21b840b7
fabmax <fabmax.thiele@gmail.com>
2024-03-24 13:52:06 +0100

    remove inclusion of kool base project

--- kool-editor-template/settings.gradle.kts
+++ kool-editor-template/settings.gradle.kts
@@ -1,6 +1,1 @@
 rootProject.name = "kool-editor-template"
-
-// include kool-libs in source to the build (requires kool-libs to be checked out at that location)
-if (File("../../kool").exists()) {
-    includeBuild("../../kool")
-}
