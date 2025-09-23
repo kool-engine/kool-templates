9212d69a0c876352dc0eefd3ef5465d2fa01b899
Oscar Fernandez <oscar.leif@gmail.com>
2025-09-23 15:28:25 -0500

    update android demo project

--- kool-android-template/app/src/main/java/de/fabmax/kool/demo/MainActivity.kt
+++ kool-android-template/app/src/main/java/de/fabmax/kool/demo/MainActivity.kt
@@ -4,12 +4,17 @@ import android.app.Activity
 import android.os.Bundle
 import de.fabmax.kool.Assets
 import de.fabmax.kool.createDefaultKoolContext
+import de.fabmax.kool.loadTexture2d
 import de.fabmax.kool.math.Vec3f
 import de.fabmax.kool.math.deg
+import de.fabmax.kool.modules.gltf.GltfLoadConfig
+import de.fabmax.kool.modules.gltf.GltfMaterialConfig
 import de.fabmax.kool.modules.gltf.loadGltfModel
 import de.fabmax.kool.modules.ksl.KslPbrShader
-import de.fabmax.kool.pipeline.ibl.EnvironmentHelper
-import de.fabmax.kool.pipeline.ibl.EnvironmentMaps
+import de.fabmax.kool.pipeline.MipMapping
+import de.fabmax.kool.pipeline.SamplerSettings
+import de.fabmax.kool.pipeline.TexFormat
+import de.fabmax.kool.pipeline.ibl.EnvironmentMap
 import de.fabmax.kool.platform.KoolContextAndroid
 import de.fabmax.kool.scene.Scene
 import de.fabmax.kool.scene.Skybox
@@ -63,32 +68,46 @@ class MainActivity : Activity() {
         launchOnMainThread {
             defaultOrbitCamera()
 
-            val ibl = EnvironmentHelper.hdriEnvironment("shanghai_bund_1k.rgbe.png")
+            val ibl = loadHdri("shanghai_bund_1k.rgbe.png")
             teapot(ibl)
             addNode(Skybox.cube(ibl.reflectionMap, 2f))
         }
     }
 
-    private suspend fun Scene.teapot(ibl: EnvironmentMaps) {
+    private suspend fun Scene.teapot(ibl: EnvironmentMap) {
+        val materialCfg = GltfMaterialConfig(
+            environmentMap = ibl
+        )
         // gzip compression and file ending are stripped from asset files (use "teapot.gltf" asset
         // path although project file is named "teapot.gltf.gz"!)
-        val teapot = Assets.loadGltfModel("teapot.gltf").apply {
-            val mesh = meshes.values.first()
-            val bounds = mesh.geometry.bounds
-            mesh.transform.translate(bounds.center * -1f)
-            mesh.shader = KslPbrShader {
-                color { uniformColor(MdColor.PINK.toLinear()) }
-                imageBasedAmbientColor(ibl.irradianceMap)
-                reflectionMap = ibl.reflectionMap
-                roughness(0.25f)
-            }
+        val modelCfg = GltfLoadConfig(materialConfig = materialCfg)
+        val model = Assets.loadGltfModel("teapot.gltf",modelCfg).getOrThrow()
 
-            onUpdate {
-                transform.rotate(30f.deg * Time.deltaT, Vec3f.X_AXIS)
-                transform.rotate(63f.deg * Time.deltaT * sin(Time.gameTime * 0.2).toFloat(), Vec3f.Y_AXIS)
-            }
+        val mesh = model.meshes.values.first()
+        val bounds = mesh.geometry.bounds
+        mesh.transform.translate(bounds.center * -1f)
+
+        mesh.shader = KslPbrShader {
+            color { uniformColor(MdColor.PINK.toLinear()) }
+            roughness(0.25f)
+            enableImageBasedLighting(ibl)
         }
 
-        addNode(teapot)
+        model.onUpdate {
+            transform.rotate(30f.deg * Time.deltaT, Vec3f.X_AXIS)
+            transform.rotate(63f.deg * Time.deltaT * sin(Time.gameTime * 0.2).toFloat(), Vec3f.Y_AXIS)
+        }
+        addNode(model)
+    }
+
+    private suspend fun loadHdri(assetPath: String): EnvironmentMap {
+        val rgbe = Assets.loadTexture2d(
+            assetPath = assetPath,
+            format = TexFormat.RGBA,
+            mipMapping = MipMapping.Off,
+            samplerSettings = SamplerSettings().nearest()
+        ).getOrThrow()
+
+        return EnvironmentMap.fromHdriTexture(rgbe)
     }
 }
